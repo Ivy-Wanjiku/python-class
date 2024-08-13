919fde862e618e49c8bc2c4a25b8daa746ee9059
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-08-13 23:17:36 +0300

    add methods

--- web/school/api/views.py
+++ web/school/api/views.py
@@ -11,6 +11,12 @@ from rest_framework.response import Response
 class StudentListView(APIView):
     def get(self,request):
         students=Student.objects.all()
+        first_name=request.query_params.get("first_name")
+        nationality=request.query_params.get("nationality")
+        if first_name:
+            students=students.filter(first_name=first_name)
+        if nationality:
+            students=students.filter(nationality=nationality)
         serializer=StudentSerializer(students,many=True)
         return Response(serializer.data)
     def post(self,request):
@@ -21,6 +27,16 @@ class StudentListView(APIView):
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 class StudentDetailView(APIView):
+    def enroll_student(self,student,course_code):
+        course=Course.objects.get(id=course_code)
+        student.course.add(course)
+    def post(self,request,id):
+        student=Student.objects.get(id=id)
+        action=request.data.get("action")
+        if action=="enroll":
+            course_code=request.data.get("course")
+            self.enroll_student(student,course_code)
+            return  Response(status.HTTP_201_CREATED)
     def get(self,request,id):
         student=Student.objects.get(id=id)
         serializer=StudentSerializer(student)
--- web/school/classperiod/models.py
+++ web/school/classperiod/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+from course.models import Course
+from teacher.models import Teacher
+
 class ClassPeriod(models.Model):
+    classteacher=models.ManyToManyField(Teacher)
+    classcourse=models.ManyToManyField(Course)
     start_time = models.TimeField()
     end_time = models.TimeField()
     course = models.CharField(max_length=20)
